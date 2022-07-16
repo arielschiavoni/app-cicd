476e4a78c561675fc1c5866b7d628118deb5900c
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-07-16 16:05:48 +0200

    test: change lodash import

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import lodash from "lodash";
+import { eq } from "lodash-es";
 
-lodash.eq({}, {});
+console.log(eq({}, {}));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
